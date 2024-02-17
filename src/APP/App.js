1f7ca67596309da4e1a4688f534c11db560939c6
hcrj99 <hcrj99@hotmail.com>
2024-02-17 18:34:25 -0500

    footer + upgrades + media querys styles

--- src/APP/App.js
+++ src/APP/App.js
@@ -60,6 +60,24 @@ function App() {
           <button className = 'movie__item-edit'>Edit</button>
           <button className = 'movie__item-delete'>Delete</button>
         </article>
+        <article className = 'movies__container-movie-item'>
+          <h3 className = 'movie__item-title' >first movie</h3>
+          <p className = 'movie__item-description'>good movie</p>
+          <button className = 'movie__item-edit'>Edit</button>
+          <button className = 'movie__item-delete'>Delete</button>
+        </article>
+        <article className = 'movies__container-movie-item'>
+          <h3 className = 'movie__item-title' >first movie</h3>
+          <p className = 'movie__item-description'>good movie</p>
+          <button className = 'movie__item-edit'>Edit</button>
+          <button className = 'movie__item-delete'>Delete</button>
+        </article>
+        <article className = 'movies__container-movie-item'>
+          <h3 className = 'movie__item-title' >first movie</h3>
+          <p className = 'movie__item-description'>good movie</p>
+          <button className = 'movie__item-edit'>Edit</button>
+          <button className = 'movie__item-delete'>Delete</button>
+        </article>
       </section>
 
       <footer>
