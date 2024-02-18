9a6c488e354f91eb707b222ecd1c5dbc3f59dbf6
hcrj99 <hcrj99@hotmail.com>
2024-02-18 06:12:48 -0500

    add logic to crete movies

--- src/components/AddMovies/AddMovies.jsx
+++ src/components/AddMovies/AddMovies.jsx
@@ -1,18 +1,55 @@
-import React from "react";
+import React from 'react';
 
 function AddMovies () {
-    return (
-        <>
-        <div className = 'add__container'>
-          <h3 className = 'add__container-title'>Add Movies</h3>
-          <form>
-            <input type = 'text' placeholder = 'Movie title'/>
-            <textarea placeholder = 'Description movie'></textarea>
-            <input type = 'submit'/>
-          </form>
-        </div>
-        </>
-    );
+  
+  const tittleInit = 'Add Movie';
+
+  const[ movieState, setMovie ] = React.useState({
+    title : '',
+    descriptionMovie : ''
+  });
+
+  const getDataForm = (event) => {
+    event.preventDefault();// *Dont recharge web page
+    //get data form
+    let target = event.target;
+    let title = target.movieTitle.value;
+    let description = target.descriptionMovie.value;
+
+    //create movie item
+    let movie = {
+      id: new Date().getTime(),
+      title,
+      description,
+    };
+
+    setMovie(movie);
+  };
+
+  return (
+    <>
+    <div className = 'add__container'>
+      <h3 className = 'add__container-title'>{ tittleInit }</h3>
+
+      {(movieState.title && movieState.description) && 
+      <strong>{ "Create the movie: " + movieState.title}</strong>
+      }
+      
+      <form onSubmit={getDataForm}>
+        <input 
+        type = 'text' 
+        placeholder = 'Movie title'
+        name='movieTitle'
+        />
+        <textarea 
+        placeholder = 'Description movie'
+        name='descriptionMovie'
+        ></textarea>
+        <input type = 'submit'/>
+      </form>
+    </div>
+    </>
+  );
 }
 
 export { AddMovies };
--- src/components/SearchMovies/SerachMovies.jsx
+++ src/components/SearchMovies/SerachMovies.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React from 'react';
 
 function SearchMovies() {
-    return (
-        <>
-        <div className ='aside__container-search'>
-          <h3>Search movie</h3>
-          <form>
-            <input type = 'text' placeholder='Search movie'/>
-            <input type = 'submit'/>
-          </form>
-        </div>
-        </>
-    );
+
+  const title = 'Search Movie';
+
+  return (
+    <>
+    <div className ='aside__container-search'>
+      <h3>{ title }</h3>
+      <form>
+        <input type = 'text' placeholder='Search movie'/>
+        <input type = 'submit'/>
+      </form>
+    </div>
+    </>
+  );
 }
 
 export { SearchMovies };
