d2b0d97b165a33df7b98feda0cc8ba6e6aaa24b1
hcrj99 <hcrj99@hotmail.com>
2024-02-18 07:24:43 -0500

    add logic to list of movies

--- src/components/ListMovies/ListMovies.jsx
+++ src/components/ListMovies/ListMovies.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function ListMovies () {
+
+  const[moviesList, setMovieList] = React.useState([]);
+
+  useEffect(() => {
+    getSearchMovies();
+  },[]);
+
+  const getSearchMovies = () => {
+    let itemLocal = JSON.parse(localStorage.getItem('movies'));
+    setMovieList(itemLocal);
+  };
+
   return (
     <>
-    <article className = 'movies__container-movie-item'>
-      <h3 className = 'movie__item-title' >first movie</h3>
-      <p className = 'movie__item-description'>good movie</p>
-      <button className = 'movie__item-edit'>Edit</button>
-      <button className = 'movie__item-delete'>Delete</button>
-    </article>
+      {moviesList!= null ? moviesList.map( movie => {
+        return(
+          <article key={movie.id} className = 'movies__container-movie-item'>
+            <h3 className = 'movie__item-title' >{movie.title}</h3>
+            <p className = 'movie__item-description'>{movie.description}</p>
+            <button className = 'movie__item-edit'>Edit</button>
+            <button className = 'movie__item-delete'>Delete</button>
+          </article>
+        );
+      }) : <h2>Add movies please</h2>}
     </>
   );
 }
